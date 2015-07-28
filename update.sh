c9f397ebcf0004cae45ebe3cbe1dbb577bcc538e
Michael Cowgill <mcowgill@oddz.com>
2015-07-28 16:26:46 -0700

    prep for update to postgis geocoder

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
 versions=( "$@" )
 if [ ${#versions[@]} -eq 0 ]; then
-	versions=( */ )
+  versions=( */ )
 fi
 versions=( "${versions[@]%/}" )
 
@@ -15,15 +15,15 @@ packages="$(echo "$packagesUrl" | sed -r 's/[^a-zA-Z.-]+/-/g')"
 curl -sSL "${packagesUrl}.bz2" | bunzip2 > "$packages"
 
 for version in "${versions[@]}"; do
-	IFS=- read pg_major postgis_major <<< "$version"
-	fullVersion="$(grep -m1 -A10 "^Package: postgresql-$pg_major-postgis-$postgis_major\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
-	[ -z "$fullVersion" ] && { echo >&2 "Unable to find package for PostGIS $postgis_major on Postgres $pg_major"; exit 1; }
-	(
-		set -x
-		cp Dockerfile.template initdb-postgis.sh README.md "$version/"
-		mv "$version/Dockerfile.template" "$version/Dockerfile"
-		sed -i 's/%%PG_MAJOR%%/'$pg_major'/g; s/%%POSTGIS_MAJOR%%/'$postgis_major'/g; s/%%POSTGIS_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
-	)
+  IFS=- read pg_major postgis_major <<< "$version"
+  fullVersion="$(grep -m1 -A10 "^Package: postgresql-$pg_major-postgis-$postgis_major\$" "$packages" | grep -m1 '^Version: ' | cut -d' ' -f2)"
+  [ -z "$fullVersion" ] && { echo >&2 "Unable to find package for PostGIS $postgis_major on Postgres $pg_major"; exit 1; }
+  (
+  set -x
+  cp Dockerfile.template initdb-postgis.sh README.md "$version/"
+  mv "$version/Dockerfile.template" "$version/Dockerfile"
+  sed -i 's/%%PG_MAJOR%%/'$pg_major'/g; s/%%POSTGIS_MAJOR%%/'$postgis_major'/g; s/%%POSTGIS_VERSION%%/'$fullVersion'/g' "$version/Dockerfile"
+  )
 done
 
 rm "$packages"
